7f31b78b472d47f5a9175f8670f7336716307029
abmutungi <abmutungi@gmail.com>
2022-01-26 16:10:35 +0000

    first

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import ("fmt"
+		"encoding/json"
+)
 
+type Groupie struct{ 
+
+Artist struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
 }
+}
+
+func main() {
+		response, err := http.Get("https://groupietrackers.herokuapp.com/api")
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+	
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		var responseObject Response
+		json.Unmarshal(responseData, &responseObject)
+	
+		// fmt.Println(responseObject.Name)
+		// fmt.Println(len(responseObject.Pokemon))
+	
+		// // for i := 0; i < len(responseObject.Pokemon); i++ {
+		// //     fmt.Println(responseObject.Pokemon[i].Species.Name)
+		// // }
+	
+	}
