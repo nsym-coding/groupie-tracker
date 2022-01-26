9923e34c749b9ee13677171ad65c0ded2140b206
abmutungi <abmutungi@gmail.com>
2022-01-26 17:04:20 +0000

    unmarshalled json

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import ("fmt"
 		"encoding/json"
+		"log"
+		"os"
+		"io/ioutil"
+		"net/http"
 )
 
-type Groupie struct{ 
+// type Groupie struct{ 
 
-Artist struct {
+type Artists struct {
+
 	ID           int      `json:"id"`
 	Image        string   `json:"image"`
 	Name         string   `json:"name"`
@@ -17,10 +22,32 @@ Artist struct {
 	ConcertDates string   `json:"concertDates"`
 	Relations    string   `json:"relations"`
 }
+
+type Locations struct {
+	Index []struct {
+		ID        int      `json:"id"`
+		Locations []string `json:"locations"`
+		Dates     string   `json:"dates"`
+	} `json:"index"`
 }
 
+type Dates struct {
+	Index []struct {
+		ID    int      `json:"id"`
+		Dates []string `json:"dates"`
+	} `json:"index"`
+}
+
+type Relations struct {
+	Index []struct{
+		ID int 			`json:"id"`
+		DatesLocations map[string][]string 	`json:"DatesLocations`
+	}
+}
+
+// }
 func main() {
-		response, err := http.Get("https://groupietrackers.herokuapp.com/api")
+		response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 		if err != nil {
 			fmt.Print(err.Error())
 			os.Exit(1)
@@ -30,15 +57,19 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		//fmt.Println(string(responseData))
 	
-		var responseObject Response
-		json.Unmarshal(responseData, &responseObject)
+		var responseObject []Artists
 	
-		// fmt.Println(responseObject.Name)
-		// fmt.Println(len(responseObject.Pokemon))
-	
-		// // for i := 0; i < len(responseObject.Pokemon); i++ {
-		// //     fmt.Println(responseObject.Pokemon[i].Species.Name)
-		// // }
-	
+		err = json.Unmarshal(responseData, &responseObject)
+		if err != nil {
+		fmt.Println(err)
+			}
+		//fmt.Printf("%+v\n", responseObject)
+		fmt.Println(responseObject)
+
+	// 	 for i := 0; i < len(responseObject.Artists); i++ {
+    //    	 fmt.Println(responseObject.Artists[i])
+    //  }
 	}
