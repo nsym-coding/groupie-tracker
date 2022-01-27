be555e316889e184135312421fb26ca6faf16ee4
nsym_coding <nsym.coding@outlook.com>
2022-01-27 18:43:42 +0000

    unmarshaled all of the structs

--- main.go
+++ main.go
@@ -7,7 +7,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 )
+
+/*This var is a pointer towards template.Template that is a
+pointer to help process the html.*/
+var tpl *template.Template
+
+/*This init function, once it's initialised, makes it so that each html file
+in the templates folder is parsed i.e. they all get looked through once and
+then stored in the memory ready to go when needed*/
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*html"))
+}
 
 type Artists struct {
 	ID           int      `json:"id"`
@@ -21,7 +33,7 @@ type Artists struct {
 	Relations    string   `json:"relations"`
 }
 
-func (a Artists) BandName() (string, []string, int) {
+func (a Artists) ArtistInfo() (string, []string, int) {
 	return a.Name, a.Members, a.CreationDate
 
 }
@@ -33,20 +45,51 @@ type Locations struct {
 		Dates     string   `json:"dates"`
 	} `json:"index"`
 }
+
 type Dates struct {
 	Index []struct {
 		ID    int      `json:"id"`
 		Dates []string `json:"dates"`
 	} `json:"index"`
 }
+
 type Relation struct {
 	Index []struct {
 		ID             int                 `json:"id"`
 		DatesLocations map[string][]string `json:"datesLocations"`
-	}
+	} `json:"index"`
 }
 
-func main() {
+func unmarshalRelations() {
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/relation")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+	defer response.Body.Close()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonData := Relation{}
+	// fmt.Printf("this is type %T \n", jsonData)
+
+	err = json.Unmarshal(responseData, &jsonData)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(jsonData.Index); i++ {
+		fmt.Println(jsonData.Index[i])
+
+	}
+	//fmt.Println(jsonData)
+}
+
+func unmarshalArtists() {
 
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 
@@ -76,7 +119,108 @@ func main() {
 	//a := Artists{Name: `json:"name"`}
 
 	for i := 0; i < len(jsonData); i++ {
-		fmt.Println(jsonData[i].BandName())
+		fmt.Println(jsonData[i].ArtistInfo())
 
 	}
+
+}
+
+func unmarshalDates() {
+
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/dates")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+	defer response.Body.Close()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonData := Dates{}
+
+	err = json.Unmarshal(responseData, &jsonData)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(jsonData)
+
+	//fmt.Println(jsonData.BandName)
+
+	//a := Artists{Name: `json:"name"`}
+
+	for i := 0; i < len(jsonData.Index); i++ {
+		fmt.Println(jsonData.Index[i])
+
+	}
+
+}
+
+func unmarshalLocations() {
+
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/locations")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+	defer response.Body.Close()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonData := Locations{}
+
+	err = json.Unmarshal(responseData, &jsonData)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// fmt.Printf("type of this %T:", jsonData)
+
+	//fmt.Println(jsonData.Index[1])
+
+	//fmt.Println(jsonData.BandName)
+
+	//a := Artists{Name: `json:"name"`}
+
+	for i := 0; i < len(jsonData.Index); i++ {
+		fmt.Println(jsonData.Index[i])
+
+	}
+
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.Error(w, "404 address not found: wrong address entered!", http.StatusNotFound)
+	} else {
+		tpl.ExecuteTemplate(w, "index.html", nil)
+	}
+}
+
+func requests() {
+	fs := http.FileServer(http.Dir("./templates"))
+
+	http.ListenAndServe(":8080", nil)
+	http.Handle("/", fs)
+	http.HandleFunc("/index.html", homePage)
+}
+
+func main() {
+	//unmarshalRelations()
+	unmarshalLocations()
+	//unmarshalDates()
+	//unmarshalArtists()
+
+	//requests()
 }
