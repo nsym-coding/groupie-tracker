102603d10ca7de378a4e2516bf7bb5dc5002f7ca
nsym_coding <nsym.coding@outlook.com>
2022-01-28 10:15:23 +0000

    think found a better way to access the info

--- main.go
+++ main.go
@@ -5,9 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
 )
 
 /*This var is a pointer towards template.Template that is a
@@ -21,9 +19,19 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*html"))
 }
 
-var jsonData []Artists
+var (
+	artistID              []int
+	artistImage           []string
+	artistName            []string
+	artistMembers         []string
+	artistCreationDate    []int
+	artistFirstAlbum      []string
+	artistLocations       []string
+	artistConcertDates    []string
+	artistsDatesLocations map[string][]string
+)
 
-type Artists struct {
+type Artists []struct {
 	ID           int      `json:"id"`
 	Image        string   `json:"image"`
 	Name         string   `json:"name"`
@@ -34,13 +42,6 @@ type Artists struct {
 	ConcertDates string   `json:"concertDates"`
 	Relations    string   `json:"relations"`
 }
-
-func (a Artists) ArtistInfo() string {
-	return a.Image
-
-}
-
-var sArtists []Artists
 
 type Locations struct {
 	Index []struct {
@@ -64,199 +65,125 @@ type Relation struct {
 	} `json:"index"`
 }
 
-func unmarshalRelations() {
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/relation")
-
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-	defer response.Body.Close()
-
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	jsonData := Relation{}
-	// fmt.Printf("this is type %T \n", jsonData)
-
-	err = json.Unmarshal(responseData, &jsonData)
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < len(jsonData.Index); i++ {
-		fmt.Println(jsonData.Index[i])
-
-	}
-	//fmt.Println(jsonData)
-}
-
-func unmarshalArtists() {
-
+func artistsData(string) {
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
-
 	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+		panic("Couldn't get info for artists!")
 	}
 	defer response.Body.Close()
 
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
+		panic("Couldn't read data for artists!")
 	}
 
-	jsonData := []Artists{}
+	var responseObject Artists
+	json.Unmarshal(responseData, &responseObject)
 
-	err = json.Unmarshal(responseData, &jsonData)
+	for i := 0; i < len(responseObject); i++ {
 
-	if err != nil {
-		panic(err)
+		artistID = append(artistID, responseObject[i].ID)
 	}
 
-	//fmt.Println(jsonData)
+	for i := 0; i < len(responseObject); i++ {
 
-	fmt.Println(jsonData[0:2])
+		artistImage = append(artistImage, responseObject[i].Image)
+	}
+	for i := 0; i < len(responseObject); i++ {
 
-	//a := Artists{Name: `json:"name"`}
+		artistName = append(artistName, responseObject[i].Name)
+	}
+	for i := 0; i < len(responseObject); i++ {
 
-	// for i := 0; i < len(jsonData); i++ {
-	// 	fmt.Println(jsonData[i].ArtistInfo())
+		artistMembers = append(artistMembers, responseObject[i].Members...)
+	}
+	for i := 0; i < len(responseObject); i++ {
 
-	// }
+		artistCreationDate = append(artistCreationDate, responseObject[i].CreationDate)
+	}
+	for i := 0; i < len(responseObject); i++ {
+
+		artistFirstAlbum = append(artistFirstAlbum, responseObject[i].FirstAlbum)
+	}
+	for i := 0; i < len(responseObject); i++ {
+
+		artistLocations = append(artistLocations, responseObject[i].Locations)
+	}
+	for i := 0; i < len(responseObject); i++ {
+
+		artistConcertDates = append(artistConcertDates, responseObject[i].ConcertDates)
+	}
+	//fmt.Println(artistName[0])
 
 }
 
-func unmarshalDates() {
-
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/dates")
-
+func main() {
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+		panic("Couldn't get info for artists!")
 	}
 	defer response.Body.Close()
 
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
+		panic("Couldn't read data for artists!")
 	}
 
-	jsonData := Dates{}
+	var responseObject Artists
+	json.Unmarshal(responseData, &responseObject)
 
-	err = json.Unmarshal(responseData, &jsonData)
+	for i := 0; i < len(responseObject); i++ {
 
-	if err != nil {
-		panic(err)
+		artistID = append(artistID, responseObject[i].ID)
 	}
 
-	fmt.Println(jsonData)
+	for i := 0; i < len(responseObject); i++ {
 
-	//fmt.Println(jsonData.BandName)
-
-	//a := Artists{Name: `json:"name"`}
-
-	// for i := 0; i < len(jsonData.Index); i++ {
-	// 	fmt.Println(jsonData.Index[i])
-
-	// }
-
-}
-
-func unmarshalLocations() {
-
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/locations")
-
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+		artistImage = append(artistImage, responseObject[i].Image)
 	}
-	defer response.Body.Close()
+	for i := 0; i < len(responseObject); i++ {
 
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
+		artistName = append(artistName, responseObject[i].Name)
 	}
+	for i := 0; i < len(responseObject); i++ {
 
-	jsonData := Locations{}
+		artistMembers = append(artistMembers, responseObject[i].Members...)
+	}
+	for i := 0; i < len(responseObject); i++ {
 
-	err = json.Unmarshal(responseData, &jsonData)
+		artistCreationDate = append(artistCreationDate, responseObject[i].CreationDate)
+	}
+	for i := 0; i < len(responseObject); i++ {
 
-	if err != nil {
-		panic(err)
+		artistFirstAlbum = append(artistFirstAlbum, responseObject[i].FirstAlbum)
+	}
+	for i := 0; i < len(responseObject); i++ {
+
+		artistLocations = append(artistLocations, responseObject[i].Locations)
+	}
+	for i := 0; i < len(responseObject); i++ {
+
+		artistConcertDates = append(artistConcertDates, responseObject[i].ConcertDates)
 	}
 
-	// fmt.Printf("type of this %T:", jsonData)
-
-	//fmt.Println(jsonData.Index[1])
-
-	//fmt.Println(jsonData.BandName)
-
-	//a := Artists{Name: `json:"name"`}
-
-	for i := 0; i < len(jsonData.Index); i++ {
-		fmt.Println(jsonData.Index[i])
-
-	}
+	//fmt.Println(artistImage[0:5])
+	requests()
 
 }
 
 func requests() {
 	fs := http.FileServer(http.Dir("./templates"))
 
-	http.ListenAndServe(":8080", nil)
-	http.Handle("/", fs)
-	http.HandleFunc("/index.html", index)
-}
-
-func main() {
-	//unmarshalRelations()
-	//unmarshalLocations()
-	//unmarshalDates()
-	//unmarshalArtists()
-
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
-
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-	defer response.Body.Close()
-
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	jsonData := []Artists{}
-
-	err = json.Unmarshal(responseData, &jsonData)
-
-	if err != nil {
-		panic(err)
-	}
-
-	//fmt.Println(jsonData)
-
-	fmt.Println(jsonData[0:2])
-
-	fmt.Println()
-
-	fs := http.FileServer(http.Dir("./templates"))
-
 	http.Handle("/", fs)
 	http.HandleFunc("/index.html", index)
 	http.ListenAndServe(":8080", nil)
 }
-
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(jsonData)
+	fmt.Println(artistImage[0:5])
 	if r.URL.Path != "/" {
 		http.Error(w, "404 address not found: wrong address entered!", http.StatusNotFound)
 	} else {
 
-		tpl.ExecuteTemplate(w, "index.html", jsonData[0])
+		tpl.ExecuteTemplate(w, "index.html", artistImage)
 	}
 }
