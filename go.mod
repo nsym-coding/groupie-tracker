4d7b42fa834992da8ca3c9c0806e943fa638108e
nsym_coding <nsym_coding@outlook.com>
2022-02-04 03:13:03 +0000

    -

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module git.learn.01founders.co/nsym_coding/groupie-tracker.git
 
 go 1.17
+
+require golang.org/x/tools/gopls v0.7.5 // indirect
--- main.go
+++ main.go
@@ -17,56 +17,6 @@ pointer to help process the html.*/
 // 	tpl = template.Must(template.ParseGlob("templates/*html"))
 // }
 
-// var (
-// 	ArtistID              []int
-// 	ArtistImage           []string
-// 	ArtistName            []string
-// 	ArtistMembers         [][]string
-// 	ArtistCreationDate    []int
-// 	ArtistFirstAlbum      []string
-// 	ArtistLocations       [][]string
-// 	ArtistConcertDates    [][]string
-// 	ArtistsDatesLocations map[string][]string
-// )
-
-// type Artists []struct {
-// 	ID           int      `json:"id"`
-// 	Image        string   `json:"image"`
-// 	Name         string   `json:"name"`
-// 	Members      []string `json:"members"`
-// 	CreationDate int      `json:"creationDate"`
-// 	FirstAlbum   string   `json:"firstAlbum"`
-// 	Locations    string   `json:"locations"`
-// 	ConcertDates string   `json:"concertDates"`
-// 	Relations    string   `json:"relations"`
-// }
-
-// type Dates struct {
-// 	Dates []dates `json:"index"`
-// }
-
-// type dates struct {
-// 	ID    int      `json:"id"`
-// 	Dates []string `json:"dates"`
-// }
-// type Locations struct {
-// 	Locations []locations `json:"index"`
-// }
-
-// type locations struct {
-// 	ID        int      `json:"id"`
-// 	Locations []string `json:"locations"`
-// 	Dates     string   `json:"dates"`
-// }
-
-// type Relation struct {
-// 	Relation []relations `json:"index"`
-// }
-
-// type relations struct {
-// 	ID             int                 `json:"id"`
-// 	DatesLocations map[string][]string `json:"datesLocations"`
-// }
 
 func main() {
 
@@ -93,59 +43,6 @@ func main() {
 // }
 // func index(w http.ResponseWriter, r *http.Request) {
 
-// 	//--------------Unmarshall Artists-------------------
-// 	responseArtists, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
-// 	if err != nil {
-// 		panic("Couldn't get Artists info from API")
-// 	}
-// 	defer responseArtists.Body.Close()
-
-// 	responseArtistsData, err := ioutil.ReadAll(responseArtists.Body)
-// 	if err != nil {
-// 		panic("Couldn't read data for Artists!")
-// 	}
-
-// 	var responseObjectArtists []Artists
-// 	json.Unmarshal(responseArtistsData, &responseObjectArtists)
-
-// 	for i := 0; i < len(responseObjectArtists); i++ {
-// 		ArtistID = append(ArtistID, ArtistID[i])
-// 	}
-
-// 	//--------------Unmarshall Locations-------------------
-
-// 	responseLocations, err := http.Get("https://groupietrackers.herokuapp.com/api/locations")
-// 	if err != nil {
-// 		panic("Couldn't get Location info from API")
-// 	}
-// 	defer responseLocations.Body.Close()
-
-// 	responseLocationsData, err := ioutil.ReadAll(responseLocations.Body)
-// 	if err != nil {
-// 		panic("Couldn't read data for Locations!")
-// 	}
-
-// 	var responseObjectLocations []Locations
-// 	json.Unmarshal(responseLocationsData, &responseObjectLocations)
-
-// 	for i := 0; i < len(responseObjectLocations); i++ {
-// 		fmt.Println(responseObjectLocations[i])
-// 	}
-
-// 	//--------------Unmarshall Dates-------------------
-// 	responseDates, err := http.Get("https://groupietrackers.herokuapp.com/api/dates")
-// 	if err != nil {
-// 		panic("Couldn't get Dates info from the API!")
-// 	}
-// 	defer responseDates.Body.Close()
-
-// 	responseDatesData, err := ioutil.ReadAll(responseDates.Body)
-// 	if err != nil {
-// 		panic("Couldn't read data for Dates")
-// 	}
-
-// 	var responseObjectDates Dates
-// 	json.Unmarshal(responseDatesData, &responseObjectDates)
 
 // 	//-------------Create a struct to hold unmarshalled data-----------
 
